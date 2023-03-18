ccf4504edd20bfe2c853eff557109acdece509a8
Murad <agamedov94@mail.ru>
2023-03-18 17:47:08 +0400

    services__List

--- script/script.js
+++ script/script.js
@@ -53,3 +53,40 @@ $('.team__slider .slider').slick({
      }
    ]
 });
+
+
+
+
+const up =  document.querySelector(".up");
+window.addEventListener("scroll", () => {
+  if(window.scrollY>900) {
+    up.style.display = "block";
+  } else {
+    up.style.display = "none";
+  }
+});
+
+up.addEventListener('click', () => {
+  const duration = 500;
+  const startingY = window.pageYOffset;
+  const elementY = 0;
+  
+
+  function step(timestamp) {
+    const progress = Math.min(1, (timestamp - start) / duration); 
+    const ease = easeInOutQuad(progress);
+    window.scrollTo(0, startingY + (elementY - startingY) * ease); 
+    if (progress < 1) {
+      window.requestAnimationFrame(step); 
+    }
+  }
+  
+
+  function easeInOutQuad(progress) {
+    return progress < 0.5 ? 2 * progress * progress : 1 - Math.pow(-2 * progress + 2, 2) / 2;
+  }
+  
+
+  const start = performance.now(); 
+  window.requestAnimationFrame(step);
+});
