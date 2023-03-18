4969811e10ea6ea936505a13c454b056190a2504
Murad <agamedov94@mail.ru>
2023-03-18 14:45:46 +0400

    ourteam

--- script/script.js
+++ script/script.js
@@ -24,3 +24,32 @@ $('.services__slider .slider').slick({
       }
     ]
  });
+
+
+
+ 
+$('.team__slider .slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  centerMode: false,
+  slidesToScroll: 1,
+
+  prevArrow: '<button type="button" class="slider__arrow team__slider__arrow--prev"><i class="fa fa-angle-right"></i></button>',
+  nextArrow: '<button type="button" class="slider__arrow team__slider__arrow--next"><i class="fa fa-angle-left"></i></button>',
+  responsive: [
+     {
+       breakpoint: 788,
+       settings: {
+         slidesToShow: 1,
+         arrows: false
+       }
+     },
+     {
+       breakpoint: 1200,
+       settings: {
+         slidesToShow: 2,
+         arrows: false
+       }
+     }
+   ]
+});
